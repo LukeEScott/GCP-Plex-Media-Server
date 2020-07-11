159144e922e2e4f5a2642c227f67dcd1468beb0e
LukeEScott <lukeescott@MacBook-Pro.local>
2020-07-11 16:41:30 +0100

    Added Variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+// Variables
+variable "project" {
+description = "The name of the GCP project."
+default = "development-test-282920"
+}
+
+variable "region" {
+  description = "The region of the GCP project."
+  default = "europe-west2"
+}
+
+variable "zone" {
+  description = "The zone of the GCP project."
+  default = "europe-west2-c"
+}
